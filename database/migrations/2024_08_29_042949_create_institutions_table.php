1e0d9a5bc73a1ba2a40b015b0ddea362e23b37c5
Abu Ubaida <rakib1515hassan@gmail.com>
2024-08-29 10:42:25 +0600

    update

--- database/migrations/2024_08_29_042949_create_institutions_table.php
+++ database/migrations/2024_08_29_042949_create_institutions_table.php
@@ -14,6 +14,14 @@ return new class extends Migration
         Schema::create('institutions', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('name');
+            $table->string('address');
+            $table->date('start');
+            $table->date('end')->nullable();
+            $table->string('roll');
+            $table->boolean('running')->default(false);
         });
     }
 
--- laravelNote.md
+++ laravelNote.md
@@ -10,3 +10,9 @@
 # Create Migrations Table
 
     >> php artisan make:migration create_<table_name>_table
+
+    >> php artisan migrate
+
+# Specipic Migrations
+    >>  php artisan migrate --path=<Path>/<Migration Name>
+    EX: php artisan migrate --path=database/migrations/2024_04_17_123039_create_memberships_table.php
